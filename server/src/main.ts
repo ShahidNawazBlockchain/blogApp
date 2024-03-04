416e6d42e1e556f5c1a79ecb768da488ad68b6ca
Zahid Nawaz <151016187+ShahidNawazBlockchain@users.noreply.github.com>
2024-03-04 05:06:44 -0800

    ...

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors();
+  // Define CORS options
+  const corsOptions: CorsOptions = {
+    origin: [
+      'http://localhost:3001',
+      'http://localhost:3000'
+    ], // or specify your frontend URL(s) here
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  };
+  // Enable CORS with options
+  app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3002);
+  const port: number = 3006;
+  await app.listen(port);
+  console.log(`http://localhost:${port}`);
 }
 bootstrap();
